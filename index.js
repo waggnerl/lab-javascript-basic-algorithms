dae3a70e87e688e22d2b2de89702c34008d55afa
waggnerl <wagnermflira@gmail.com>
2023-01-17 09:05:07 +0000

    feat:Elaboration of final lab day1

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 // Iteration 1: Names and Input
-
+let hacker1 = "Wagner";
+let hacker2 = "Assebe";
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker1.length < hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker2.length} characters.`
+  );
+} else if (hacker1.length === hacker2.length) {
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.
+    `);
+}
 
 // Iteration 3: Loops
+
+let i = 0;
+
+while (i !== hacker1.length) {
+  console.log(hacker1[i]);
+  i++;
+}
+
+let b = hacker1.length - 1;
+
+while (b >= 0) {
+  console.log(hacker1[b]);
+  b--;
+}
+
+if (hacker1 < hacker2) {
+  console.log("The driver's name goes first.");
+} else if (hacker1 > hacker2) {
+  console.log("Yo, the navigator goes first definitely.");
+} else if (hacker1 === hacker2) {
+  console.log("What?! You both have the same name?");
+}
